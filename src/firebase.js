d4324d1c2aa8c7d1c83716bcc82e860738300134
Cristian Felicio <94009542+Crostiam@users.noreply.github.com>
2026-04-30 17:05:25 +0100

    Added index.html

--- src/firebase.js
+++ src/firebase.js
@@ -1,23 +1,17 @@
-<script type="module">
-  // Import the functions you need from the SDKs you need
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-analytics.js";
-  // TODO: Add SDKs for Firebase products that you want to use
-  // https://firebase.google.com/docs/web/setup#available-libraries
+import { initializeApp } from 'firebase/app';
+import { getDatabase, ref, set, onValue } from 'firebase/database';
 
-  // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-  const firebaseConfig = {
-    apiKey: "AIzaSyAM_rP5k7PAuBq8_Xkin23X9NYW5qolJsM",
-    authDomain: "ludo-14af4.firebaseapp.com",
-    projectId: "ludo-14af4",
-    storageBucket: "ludo-14af4.firebasestorage.app",
-    messagingSenderId: "1097419329945",
-    appId: "1:1097419329945:web:a517052b14f71639ea0308",
-    measurementId: "G-7B6RCVKXXK"
-  };
+const firebaseConfig = {
+  apiKey: "AIzaSyAM_rP5k7PAuBq8_Xkin23X9NYW5qolJsM",
+  authDomain: "ludo-14af4.firebaseapp.com",
+  databaseURL: "https://ludo-14af4-default-rtdb.firebaseio.com",
+  projectId: "ludo-14af4",
+  storageBucket: "ludo-14af4.firebasestorage.app",
+  messagingSenderId: "1097419329945",
+  appId: "1:1097419329945:web:a517052b14f71639ea0308",
+  measurementId: "G-7B6RCVKXXK"
+};
 
-  // Initialize Firebase
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-</script>
+const app = initializeApp(firebaseConfig);
+export const database = getDatabase(app);
+export { ref, set, onValue };
